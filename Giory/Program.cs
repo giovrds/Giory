210a3074d7403c75f644e654ebc104c14e96b5eb
giovrds <escolargiovanna@gmail.com>
2023-10-05 15:07:56 -0400

    primeiro comit

---
+++ Giory/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class github
+{
+    static void Main()
+    {
+
+        Console.Write("Digite um número....");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("\nDigite outro número....");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"\nA soma deste dois número equivale a {num1 + num2}");
+
+        Console.ReadKey();
+
+    }
+}
